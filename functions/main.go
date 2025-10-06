65f5d2422b4330410a296f43464c39bc21841de7
Samuel <samueladeyemi2006@gmail.com>
2025-10-06 17:00:41 +0100

    more on functions

--- functions/main.go
+++ functions/main.go
@@ -2,26 +2,38 @@ package main
 
 import (
 	"fmt"
-	"math"
 )
 
-func sayGreeting(n string){
-	fmt.Printf("Good morning %v \n", n)
+// func sayGreeting(n string){
+// 	fmt.Printf("Good morning %v \n", n)
+// }
+
+// func sayGoodbye(n string){
+// 	fmt.Printf("Goodbye %v \n", n)
+// }
+
+// func cycleNames(s []string, f func(string)){ //passing a function as params
+// 	for _,v := range s{
+// 		f(v)
+// 	}
+// }
+
+// func circleArea(r float64) float64 {
+// 	return math.Pi * r * r
+// }
+
+//my example
+
+func sayGoodday(name string) {
+	fmt.Printf("Good day %v \n", name)
 }
 
-func sayGoodbye(n string){
-	fmt.Printf("Goodbye %v \n", n)
+func loopName(names []string, f func(string)){
+   for _,value := range names{
+	f(value)
+   }
 }
 
-func cycleNames(s []string, f func(string)){ //passing a function as params
-	for _,v := range s{
-		f(v)
-	}
-}
-
-func circleArea(r float64) float64 {
-	return math.Pi * r * r
-}
 
 func main() {
 //    sayGreeting("Samuel")
@@ -29,10 +41,15 @@ func main() {
 //    cycleNames([]string{"ox", "ad", "sam"}, sayGreeting)
 //    cycleNames([]string{"ox", "ad", "sam"}, sayGoodbye)
 
-   a1 := circleArea(10.5)
-   a2 := circleArea(20.5)
+//    a1 := circleArea(10.5)
+//    a2 := circleArea(20.5)
 
-   fmt.Println(a1, a2) //without formatting
-   fmt.Printf("circle 1 is %0.3f circle 2 is %0.3f \n", a1, a2) //formatted to 3dp
+//    fmt.Println(a1, a2) //without formatting
+//    fmt.Printf("circle 1 is %0.3f circle 2 is %0.3f \n", a1, a2) //formatted to 3dp
+
+
+   //my examples
+   sayGoodday("sam")
+   loopName([]string{"ad", "israel", "jacob", "marie"}, sayGoodday)
 }
 
