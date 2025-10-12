b7d643428c50f408a9ae9f2a21f153053b9f4920
Samuel <samueladeyemi2006@gmail.com>
2025-10-12 07:24:50 +0100

    go package scope

---
+++ package-scope/greeting.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+
+var points = []int{2,4,5,6,7,9,14}
+
+func sayHello(n string) {
+	fmt.Printf("Hello %v \n", n)
+
+	fmt.Println(name)
+}
---
+++ package-scope/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+var name = "AD"
+
+func main() {
+	sayHello("samuel")
+
+	for _,v := range points {
+		fmt.Println(v)
+	}
+}
